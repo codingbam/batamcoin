279d464c1526b5ffb106eefebb2519e8d885f8c9
Visionmeks <madinahdapur10@gmail.com>
2019-04-13 10:24:17 +0700

    batamcoin

--- src/qt/locale/bitcoin_bs.ts
+++ src/qt/locale/bitcoin_bs.ts
@@ -14,8 +14,8 @@
 <context>
     <name>BitcoinGUI</name>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>Batamcoin</source>
+        <translation>Batamcoin</translation>
     </message>
     </context>
 <context>
@@ -33,8 +33,8 @@
 <context>
     <name>Intro</name>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>Batamcoin</source>
+        <translation>Batamcoin</translation>
     </message>
     </context>
 <context>
@@ -160,8 +160,8 @@
 <context>
     <name>bitcoin-core</name>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Jezrga</translation>
+        <source>Batamcoin Core</source>
+        <translation>Batamcoin Jezrga</translation>
     </message>
     </context>
 </TS>
